85fec5c1ea5355adf417e46a0d56bceecf349fea
tyler0501 <tyler920501@gmail.com>
2023-10-12 22:21:46 +0800

    1012

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,10 @@
+import androidx.fragment.app.FragmentActivity
+import android.os.Bundle
+
+class MyActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 這裡放入你的程式碼
+    }
+}
